8ef4fb85bb0630e6ae3d6ed972ba99d525c419c4
Joseph D Goelz <jgoelz@usc.edu>
2016-01-18 21:04:50 -0800

    Enabled mvc actions

--- src/JosephGoelz/Startup.cs
+++ src/JosephGoelz/Startup.cs
@@ -13,10 +13,16 @@ namespace JosephGoelz
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseMvc(routes =>
+                routes.MapRoute("Default",
+                    "{controller=Home}/{action=Index}/{id?}"));
+
+            app.UseFileServer();
             /*
             app.Use(async (context, next) =>
             {
